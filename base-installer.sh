93cc8d987b371551d08bec59f5e91fd0666f82c9
Rakibul Hasan Ratul <rakibulhasanratul@proton.me>
2026-08-21 16:44:21 +0600

    feat: enforce zstd3 compression

--- base-installer.sh
+++ base-installer.sh
@@ -17,15 +17,15 @@ Now this will execute:
   btrfs su cr /mnt/@log
   btrfs su cr /mnt/@snapshots
   umount /mnt
-  mount -o noatime,compress=zstd,subvol=@ /dev/nvme0n1p3 /mnt
+  mount -o noatime,compress-force=zstd:3,subvol=@ /dev/nvme0n1p3 /mnt
   mkdir -p /mnt/{boot,home,var/log,var/cache/pacman/pkg,.snapshots}
   mount /dev/nvme0n1p2 /mnt/boot
   mkdir -p /mnt/boot/efi
   mount /dev/nvme0n1p1 /mnt/boot/efi
-  mount -o noatime,compress=zstd,subvol=@log /dev/nvme0n1p3 /mnt/var/log
-  mount -o noatime,compress=zstd,subvol=@pkg /dev/nvme0n1p3 /mnt/var/cache/pacman/pkg
-  mount -o noatime,compress=zstd,subvol=@snapshots /dev/nvme0n1p3 /mnt/.snapshots
-  mount -o noatime,compress=zstd,subvol=@home /dev/nvme0n1p4 /mnt/home
+  mount -o noatime,compress-force=zstd:3,subvol=@log /dev/nvme0n1p3 /mnt/var/log
+  mount -o noatime,compress-force=zstd:3,subvol=@pkg /dev/nvme0n1p3 /mnt/var/cache/pacman/pkg
+  mount -o noatime,compress-force=zstd:3,subvol=@snapshots /dev/nvme0n1p3 /mnt/.snapshots
+  mount -o noatime,compress-force=zstd:3,subvol=@home /dev/nvme0n1p4 /mnt/home
 EOF
 
 mkfs.fat -F32 /dev/nvme0n1p1
@@ -37,15 +37,15 @@ btrfs su cr /mnt/@pkg
 btrfs su cr /mnt/@log
 btrfs su cr /mnt/@snapshots
 umount /mnt
-mount -o noatime,compress=zstd,subvol=@ /dev/nvme0n1p3 /mnt
+mount -o noatime,compress-force=zstd:3,subvol=@ /dev/nvme0n1p3 /mnt
 mkdir -p /mnt/{boot,home,var/log,var/cache/pacman/pkg,.snapshots}
 mount /dev/nvme0n1p2 /mnt/boot
 mkdir -p /mnt/boot/efi
 mount /dev/nvme0n1p1 /mnt/boot/efi
-mount -o noatime,compress=zstd,subvol=@log /dev/nvme0n1p3 /mnt/var/log
-mount -o noatime,compress=zstd,subvol=@pkg /dev/nvme0n1p3 /mnt/var/cache/pacman/pkg
-mount -o noatime,compress=zstd,subvol=@snapshots /dev/nvme0n1p3 /mnt/.snapshots
-mount -o noatime,compress=zstd,subvol=@home /dev/nvme0n1p4 /mnt/home
+mount -o noatime,compress-force=zstd:3,subvol=@log /dev/nvme0n1p3 /mnt/var/log
+mount -o noatime,compress-force=zstd:3,subvol=@pkg /dev/nvme0n1p3 /mnt/var/cache/pacman/pkg
+mount -o noatime,compress-force=zstd:3,subvol=@snapshots /dev/nvme0n1p3 /mnt/.snapshots
+mount -o noatime,compress-force=zstd:3,subvol=@home /dev/nvme0n1p4 /mnt/home
 
 echo 'Installing base system'
 pacstrap -K /mnt base base-devel linux-lts linux-lts-headers linux-firmware btrfs-progs networkmanager sudo nano vim git fish neovim grub wget curl gvfs gvfs-mtp mtpfs libmtp tree-sitter-cli efibootmgr
